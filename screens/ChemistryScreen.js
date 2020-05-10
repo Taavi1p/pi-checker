a88225cc6205e599af644075652be12cb35fe3b1
Kadri Põldmaa <kadripoldmaa@iMac.lan>
2020-05-10 15:03:04 +0300

    haha

--- screens/ChemistryScreen.js
+++ screens/ChemistryScreen.js
@@ -7,7 +7,11 @@ const ChemistryScreen = props => {
     return (
         <View style={styles.screen}>
             <ScrollView style={{paddingTop: 50}}>
-                <MenuButton style={styles.physics}>PERIODIC TABLE</MenuButton>
+                <MenuButton style={styles.periodic}>PERIODIC TABLE</MenuButton>
+                <MenuButton style={styles.element}>NAME THE ELEMENT</MenuButton>
+                <MenuButton style={styles.chemist}>NAME THE CHEMIST</MenuButton>
+                <MenuButton style={styles.molecul}>NAME THE MOLECUL</MenuButton>
+                <MenuButton style={styles.symbol}>NAME THE SYMBOL</MenuButton>
             </ScrollView>
         </View>
     )
@@ -16,9 +20,22 @@ const ChemistryScreen = props => {
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        backgroundColor: 'black',
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: '#9933ff',
+    },
+    periodic: {
+        backgroundColor: '#a64dff',
+    },
+    element: {
+        backgroundColor: '#8c1aff',
+    },
+    chemist: {
+        backgroundColor: '#7300e6',
+    },
+    molecul: {
+        backgroundColor: '#5900b3',
+    },
+    symbol: {
+        backgroundColor: '#400080',
     }
 })
 
--- screens/MathScreen.js
+++ screens/MathScreen.js
@@ -6,8 +6,12 @@ import MenuButton from '../components/MenuButton'
 const MathScreen = props => {
     return (
         <View style={styles.screen}>
-            <ScrollView style={{paddingTop: 50}}>
-                <MenuButton style={styles.physics}>PI CHECKER</MenuButton>
+            <ScrollView style={{paddingTop: 20}}>
+                <MenuButton style={styles.pi}>PI CHECKER</MenuButton>
+                <MenuButton style={styles.i}>I CHECKER</MenuButton>
+                <MenuButton style={styles.e}>E CHECKER</MenuButton>
+                <MenuButton style={styles.shape}>GUESS THE SHAPE</MenuButton>                
+                <MenuButton style={styles.equation}>NAME THE EQUATION</MenuButton>                
             </ScrollView>
         </View>
     )
@@ -16,9 +20,22 @@ const MathScreen = props => {
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        backgroundColor: 'black',
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: '#1439ff',
+    },
+    pi: {
+        backgroundColor: '#4d79ff'
+    },
+    i: {
+        backgroundColor: '#1a53ff'
+    },
+    e: {
+        backgroundColor: '#0039e6'
+    },
+    shape: {
+        backgroundColor: '#002db3'
+    },
+    equation: {
+        backgroundColor: '#002080'
     }
 })
 
